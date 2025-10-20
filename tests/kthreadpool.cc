c153f532ff418d466faafc308e408033a9b81409
Andre Wagner <andre.nho@gmail.com>
2025-10-20 09:39:58 -0500

    .

--- tests/kthreadpool.cc
+++ tests/kthreadpool.cc
@@ -1,5 +1,6 @@
 #include "doctest.h"
 
+#include <atomic>
 #include <chrono>
 #include <thread>
 
@@ -11,22 +12,22 @@ TEST_SUITE("KThreadPool")
 {
     TEST_CASE("KThreadPool - execute ")
     {
-        size_t i = 0;
+        std::atomic<size_t> i = 0;
         {
             KThreadPool ktpool(1);
             ktpool.add_task(1, [&i]() { ++i; return true; });
-            ktpool.add_task(1, [&i]() { i *= 2; return true; });
+            ktpool.add_task(1, [&i]() { i = i * 2; return true; });
         }
         CHECK(i == 2);
     }
 
     TEST_CASE("KThreadPool - execute ")
     {
-        size_t i = 0;
+        std::atomic<size_t> i = 0;
         {
             KThreadPool ktpool(8);
             ktpool.add_task(1, [&i]() { ++i; std::this_thread::sleep_for(200ms); return true; });
-            ktpool.add_task(1, [&i]() { i *= 2; return true; });
+            ktpool.add_task(1, [&i]() { i = i * 2; return true; });
             std::this_thread::sleep_for(120ms);
             ktpool.add_task(2, [&i]() { i += 2; return true; });
         }
