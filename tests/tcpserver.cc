36e0034814b550fc636b16c6bba179b6d2f267d9
Andre Wagner <andre.nho@gmail.com>
2025-10-22 08:37:38 -0500

    .

--- tests/tcpserver.cc
+++ tests/tcpserver.cc
@@ -7,14 +7,17 @@
 #include "client/tcpclient.hh"
 
 static std::atomic<bool> server_running;
+static std::atomic<bool> server_ready;
 
 #define PORT 23456
 
 static std::thread run_server()
 {
     server_running = true;
+    server_ready = false;
     return std::thread([]() {
         auto server = TCPServer(PORT, false, std::make_unique<EchoProtocol>(), 8);
+        server_ready = true;
         while (server_running)
             server.iterate();
     });
@@ -25,7 +28,8 @@ TEST_SUITE("TCP Server")
     TEST_CASE("Simple echo")
     {
         logging_verbose = true;
-        std::thread t = run_server();  // TODO - wait until server is ready
+        std::thread t = run_server();
+        while (!server_ready) {}
 
         TCPClient client("127.0.0.1", PORT);
         client.send("hello\r\n");
